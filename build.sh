e18e898be43719128a3731d2311adf0aaf5baf65
James Judd <james@lucidchart.com>
2016-09-26 19:26:58 -0600

    Updating Angular 2's new build process to produce Closure compatible bundles in addition to normal bundles

--- build.sh
+++ build.sh
@@ -94,72 +94,90 @@ if [[ ${BUILD_ALL} == true ]]; then
   rm -rf ./dist/packages-dist
 fi
 
+
 for PACKAGE in ${PACKAGES[@]}
 do
   PWD=`pwd`
   SRCDIR=${PWD}/modules/@angular/${PACKAGE}
-  DESTDIR=${PWD}/dist/packages-dist/${PACKAGE}
-  UMD_ES5_PATH=${DESTDIR}/bundles/${PACKAGE}.umd.js
-  UMD_TESTING_ES5_PATH=${DESTDIR}/bundles/${PACKAGE}-testing.umd.js
-  UMD_ES5_MIN_PATH=${DESTDIR}/bundles/${PACKAGE}.umd.min.js
-  LICENSE_BANNER=${PWD}/modules/@angular/license-banner.txt
+  DESTDIRS=(${PWD}/dist/packages-dist/${PACKAGE}
+    ${PWD}/dist/packages-closure/${PACKAGE}
+  )
+  ISCLOSURE=false
+  for DESTDIR in ${DESTDIRS[@]}
+  do
+    rm -rf ${DESTDIR}
 
-  rm -rf ${DESTDIR}
+    if [[ ${ISCLOSURE} == true && -e ${SRCDIR}/tsconfig-closure.json ]]; then
+      echo "======      COMPILING CLOSURE: ${TSC} -p ${SRCDIR}/tsconfig-closure.json"
+      $TSC -p ${SRCDIR}/tsconfig-closure.json
+    else
+      echo "======      COMPILING: ${TSC} -p ${SRCDIR}/tsconfig-build.json        ====="
+      $TSC -p ${SRCDIR}/tsconfig-build.json
+    fi
 
-  echo "======      COMPILING: ${TSC} -p ${SRCDIR}/tsconfig-build.json        ====="
-  $TSC -p ${SRCDIR}/tsconfig-build.json
+    if [[ -e ${SRCDIR}/tsconfig-testing.json ]]; then
+      echo "======      COMPILING TESTING: ${TSC} -p ${SRCDIR}/tsconfig-testing.json"
+      $TSC -p ${SRCDIR}/tsconfig-testing.json
+    fi
 
-  cp ${SRCDIR}/package.json ${DESTDIR}/
-  cp ${PWD}/modules/@angular/README.md ${DESTDIR}/
+    cp ${SRCDIR}/package.json ${DESTDIR}/
+    cp ${PWD}/modules/@angular/README.md ${DESTDIR}/
 
-  if [[ -e ${SRCDIR}/tsconfig-testing.json ]]; then
-    echo "======      COMPILING TESTING: ${TSC} -p ${SRCDIR}/tsconfig-testing.json"
-    $TSC -p ${SRCDIR}/tsconfig-testing.json
-  fi
+    UMD_ES5_PATH=${DESTDIR}/bundles/${PACKAGE}.umd.js
+    UMD_TESTING_ES5_PATH=${DESTDIR}/bundles/${PACKAGE}-testing.umd.js
+    UMD_ES5_MIN_PATH=${DESTDIR}/bundles/${PACKAGE}.umd.min.js
+    LICENSE_BANNER=${PWD}/modules/@angular/license-banner.txt
 
-  echo "======      TSC 1.8 d.ts compat for ${DESTDIR}   ====="
-  # safely strips 'readonly' specifier from d.ts files to make them compatible with tsc 1.8
-  if [ "$(uname)" == "Darwin" ]; then
-    find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i '' -e 's/\(^ *(static |private )*\)*readonly  */\1/g'
-    find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i '' -e 's/\/\/\/ <reference types="node" \/>//g'
-    find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i '' -E 's/^( +)abstract ([[:alnum:]]+\:)/\1\2/g'
-  else
-    find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i -e 's/\(^ *(static |private )*\)*readonly  */\1/g'
-    find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i -e 's/\/\/\/ <reference types="node" \/>//g'
-    find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i -E 's/^( +)abstract ([[:alnum:]]+\:)/\1\2/g'
-  fi
+    echo "======      TSC 1.8 d.ts compat for ${DESTDIR}   ====="
+    # safely strips 'readonly' specifier from d.ts files to make them compatible with tsc 1.8
+    if [ "$(uname)" == "Darwin" ]; then
+      find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i '' -e 's/\(^ *(static |private )*\)*readonly  */\1/g'
+      find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i '' -e 's/\/\/\/ <reference types="node" \/>//g'
+      find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i '' -E 's/^( +)abstract ([[:alnum:]]+\:)/\1\2/g'
+    else
+      find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i -e 's/\(^ *(static |private )*\)*readonly  */\1/g'
+      find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i -e 's/\/\/\/ <reference types="node" \/>//g'
+      find ${DESTDIR} -type f -name '*.d.ts' -print0 | xargs -0 sed -i -E 's/^( +)abstract ([[:alnum:]]+\:)/\1\2/g'
+    fi
 
-  if [[ ${PACKAGE} == benchpress ]]; then
-    cp ${SRCDIR}/*.md ${DESTDIR}
-    cp -r ${SRCDIR}/docs ${DESTDIR}
-  fi
+    if [[ ${PACKAGE} == benchpress ]]; then
+      cp ${SRCDIR}/*.md ${DESTDIR}
+      cp -r ${SRCDIR}/docs ${DESTDIR}
+    fi
 
-  if [[ ${BUNDLE} == true && ${PACKAGE} != compiler-cli && ${PACKAGE} != benchpress ]]; then
+    if [[ ${ISCLOSURE} == false && ${BUNDLE} == true && ${PACKAGE} != compiler-cli && ${PACKAGE} != benchpress ]]; then
 
-    echo "======      BUNDLING: ${SRCDIR} ====="
-    mkdir ${DESTDIR}/bundles
+      echo "======      BUNDLING: ${SRCDIR} ====="
+      mkdir ${DESTDIR}/bundles
 
-    (
-      cd  ${SRCDIR}
-      echo "======         Rollup ${PACKAGE} index"
-      ../../../node_modules/.bin/rollup -c rollup.config.js
-      cat ${LICENSE_BANNER} > ${UMD_ES5_PATH}.tmp
-      cat ${UMD_ES5_PATH} >> ${UMD_ES5_PATH}.tmp
-      mv ${UMD_ES5_PATH}.tmp ${UMD_ES5_PATH}
-      $UGLIFYJS -c --screw-ie8 --comments -o ${UMD_ES5_MIN_PATH} ${UMD_ES5_PATH}
+      (
+        cd  ${SRCDIR}
 
+        echo "======         Rollup ${PACKAGE} index"
+        ../../../node_modules/.bin/rollup -c rollup.config.js
+        cat ${LICENSE_BANNER} > ${UMD_ES5_PATH}.tmp
+        cat ${UMD_ES5_PATH} >> ${UMD_ES5_PATH}.tmp
+        mv ${UMD_ES5_PATH}.tmp ${UMD_ES5_PATH}
+        $UGLIFYJS -c --screw-ie8 --comments -o ${UMD_ES5_MIN_PATH} ${UMD_ES5_PATH}
 
-      if [[ -e rollup-testing.config.js ]]; then
-        echo "======         Rollup ${PACKAGE} testing"
-        ../../../node_modules/.bin/rollup -c rollup-testing.config.js
-        echo "{\"main\": \"../bundles/${PACKAGE}-testing.umd.js\"}" > ${DESTDIR}/testing/package.json
-        cat ${LICENSE_BANNER} > ${UMD_TESTING_ES5_PATH}.tmp
-        cat ${UMD_TESTING_ES5_PATH} >> ${UMD_TESTING_ES5_PATH}.tmp
-        mv ${UMD_TESTING_ES5_PATH}.tmp ${UMD_TESTING_ES5_PATH}
-      fi
-    ) 2>&1 | grep -v "as external dependency"
+        if [[ -e rollup-testing.config.js ]]; then
+          echo "======         Rollup ${PACKAGE} testing"
+          ../../../node_modules/.bin/rollup -c rollup-testing.config.js
+          echo "{\"main\": \"../bundles/${PACKAGE}-testing.umd.js\"}" > ${DESTDIR}/testing/package.json
+          cat ${LICENSE_BANNER} > ${UMD_TESTING_ES5_PATH}.tmp
+          cat ${UMD_TESTING_ES5_PATH} >> ${UMD_TESTING_ES5_PATH}.tmp
+          mv ${UMD_TESTING_ES5_PATH}.tmp ${UMD_TESTING_ES5_PATH}
+        fi
+      ) 2>&1 | grep -v "as external dependency"
 
-  fi
+    fi
+
+    if [[ ${ISCLOSURE} == false ]]; then
+      ISCLOSURE=true
+    else
+      ISCLOSURE=false
+    fi
+  done
 done
 
 ./modules/@angular/examples/build.sh
