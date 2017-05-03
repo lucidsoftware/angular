0e7dbee54fa05215a6f1d4ea3576ab0c84b557aa
Kevin Reece <kevinr@lucidchart.com>
2017-05-03 14:30:18 -0600

    remove animation providers

--- packages/platform-browser/animations/src/providers.ts
+++ packages/platform-browser/animations/src/providers.ts
@@ -6,59 +6,18 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-import {AnimationBuilder} from '@angular/animations';
-import {AnimationDriver, ɵAnimationEngine as AnimationEngine, ɵAnimationStyleNormalizer as AnimationStyleNormalizer, ɵNoopAnimationDriver as NoopAnimationDriver, ɵWebAnimationsDriver as WebAnimationsDriver, ɵWebAnimationsStyleNormalizer as WebAnimationsStyleNormalizer, ɵsupportsWebAnimations as supportsWebAnimations} from '@angular/animations/browser';
-import {Injectable, NgZone, Provider, RendererFactory2} from '@angular/core';
-import {ɵDomRendererFactory2 as DomRendererFactory2} from '@angular/platform-browser';
+import {Provider} from '@angular/core';
 
-import {BrowserAnimationBuilder} from './animation_builder';
-import {AnimationRendererFactory} from './animation_renderer';
-
-@Injectable()
-export class InjectableAnimationEngine extends AnimationEngine {
-  constructor(driver: AnimationDriver, normalizer: AnimationStyleNormalizer) {
-    super(driver, normalizer);
-  }
-}
-
-export function instantiateSupportedAnimationDriver() {
-  if (supportsWebAnimations()) {
-    return new WebAnimationsDriver();
-  }
-  return new NoopAnimationDriver();
-}
-
-export function instantiateDefaultStyleNormalizer() {
-  return new WebAnimationsStyleNormalizer();
-}
-
-export function instantiateRendererFactory(
-    renderer: DomRendererFactory2, engine: AnimationEngine, zone: NgZone) {
-  return new AnimationRendererFactory(renderer, engine, zone);
-}
-
-const SHARED_ANIMATION_PROVIDERS: Provider[] = [
-  {provide: AnimationBuilder, useClass: BrowserAnimationBuilder},
-  {provide: AnimationStyleNormalizer, useFactory: instantiateDefaultStyleNormalizer},
-  {provide: AnimationEngine, useClass: InjectableAnimationEngine}, {
-    provide: RendererFactory2,
-    useFactory: instantiateRendererFactory,
-    deps: [DomRendererFactory2, AnimationEngine, NgZone]
-  }
-];
+const SHARED_ANIMATION_PROVIDERS: Provider[] = [];
 
 /**
  * Separate providers from the actual module so that we can do a local modification in Google3 to
  * include them in the BrowserModule.
  */
-export const BROWSER_ANIMATIONS_PROVIDERS: Provider[] = [
-  {provide: AnimationDriver, useFactory: instantiateSupportedAnimationDriver},
-  ...SHARED_ANIMATION_PROVIDERS
-];
+export const BROWSER_ANIMATIONS_PROVIDERS: Provider[] = [];
 
 /**
  * Separate providers from the actual module so that we can do a local modification in Google3 to
  * include them in the BrowserTestingModule.
  */
-export const BROWSER_NOOP_ANIMATIONS_PROVIDERS: Provider[] =
-    [{provide: AnimationDriver, useClass: NoopAnimationDriver}, ...SHARED_ANIMATION_PROVIDERS];
+export const BROWSER_NOOP_ANIMATIONS_PROVIDERS: Provider[] = [];
